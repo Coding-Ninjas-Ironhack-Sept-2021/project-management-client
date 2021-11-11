090852431605b5e4df5275d32c2f527188ec87f6
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-11-11 12:54:59 +0100

    auth: functionality to login (Login)

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ import Navbar from './components/navbar/Navbar';
 import ProjectDetails from './components/projects/ProjectDetails';
 import TaskDetails from './components/tasks/TaskDetails';
 import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
 
 class App extends Component {
+
+  state = {
+    isLoggedIn: false,
+    user: null
+  };
+
+  getTheUser = (userObj, loggedIn) => {
+    this.setState({
+      isLoggedIn: loggedIn,
+      user: userObj
+    });
+  };
+
+
   render() {
     return (
       <div className="App">
-       <Navbar />
+        <Navbar userData={this.state.user} userIsLoggedIn={this.state.isLoggedIn} getUser={this.getTheUser} />
         <Switch>
+          <Route exact path="/" render={props => <Login {...props} getUser={this.getTheUser} />} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/projects" component={ProjectList}/>
+          <Route exact path="/projects" component={ProjectList} />
           <Route exact path="/projects/:id" component={ProjectDetails} />
           <Route exact path="/projects/:id/tasks/:taskId" component={TaskDetails} />
         </Switch>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <nav className="nav-style">
-      <ul>
-        <li><Link to="/projects" style={{ textDecoration: 'none' }}>Projects</Link></li>
-    </ul>
-    </nav>
-  )
+class Navbar extends React.Component {
+  render() {
+    const { userData, userIsLoggedIn } = this.props;
+
+    if (userIsLoggedIn) {
+      return (
+        <nav className="nav-style">
+          <ul>
+            {userIsLoggedIn && <li>Welcome, {userData.username} </li>}
+            <li>
+              <Link to="/projects" style={{ textDecoration: 'none' }}>
+                Projects
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      );
+    } else {
+      return (
+        <div>
+          <nav className="nav-style">
+            <ul>
+              <li>
+                <Link to="/signup" style={{ textDecoration: 'none' }}>
+                  Signup
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      );
+    }
+  }
 }
 
 export default Navbar;
